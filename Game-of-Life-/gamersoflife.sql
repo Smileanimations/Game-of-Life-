58dc0705b7b26919a1f18f968a46c0db3d396f98
Roald <nihilzero28@gmail.com>
2024-09-04 10:00:34 +0200

    added table

--- Game-of-Life-/gamersoflife.sql
+++ Game-of-Life-/gamersoflife.sql
@@ -1,7 +1,13 @@
-DROP DATABASE IF EXISTS `patterns`;
+DROP DATABASE IF EXISTS `gameoflife`;
 
-CREATE DATABASE `patterns`;
+CREATE DATABASE `gameoflife`;
 
-USE `patterns`;
+USE `gameoflife`;
+
+CREATE TABLE `patterns` (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(25),
+    details VARCHAR(100)
+)
 
 
