fbd27cf6c8961a52e2cc85f90ec9dbc2aaceacc9
Mark <iksnij12@gmail.com>
2024-09-05 09:14:47 +0200

    LETTERLIJK PHP gefixt

--- Game-of-Life-/connection.php
+++ Game-of-Life-/connection.php
@@ -4,12 +4,12 @@ const SESSION_LOGGEDIN_KEY="loggedin";
 const SESSION_USERNAME_KEY="username";
 const SESSION_PASSWORD_KEY="password";
 
-$ip = '127.0.0.1';
-$username = 'bit_academy';
+$ip = 'localhost';
+$username = 'root';
 $password = 'xc6cvYYg-Q1WZ]p3';
 
 try {
-    $conn = new PDO("mysql:dbname=gameoflife;host=$ip", $username, $password); 
+    $conn = new PDO("mysql:dbname=gameoflife;host=$ip", $username); 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
