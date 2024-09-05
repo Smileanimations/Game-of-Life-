40b350c6ece5bbc3cee7120823f0447c64bcf31b
Roald <nihilzero28@gmail.com>
2024-09-05 15:40:01 +0200

    Progress on sql insertion

--- Form_request.php
+++ Form_request.php
@@ -1,7 +1,22 @@
 <?php 
+include "connection.php";
+
     $name = $_POST['patternname']; 
     $image= $_POST['image']; 
     $details = $_POST['descofpattern']; 
+
+    var_dump($_FILES);
+
+    // Create insert query and insert the new user
+$sql = "INSERT INTO patterns (title, details) VALUES (:title, :details)";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':title', $name);
+$stmt->bindParam(':details', $details);
+
+// feedback on error
+if (false === $stmt->execute()) {
+    exit("DB error: " . $stmt->errorInfo()[2] . "\n");
+}
 ?>
 
 <!DOCTYPE html>
